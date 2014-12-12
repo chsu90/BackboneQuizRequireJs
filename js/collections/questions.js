a0636b70eaa35dda1f36861992de2b82bc6860ef
chsu90 <chsu90@users.noreply.github.com>
2014-12-11 18:31:29 -0800

    Create questions.js

---
+++ js/collections/questions.js
@@ -0,0 +1,15 @@
+define(['underscore', 'backbone', 'models/question'], function(_, Backbone, Question) {
+
+	var Questions = Backbone.Collection.extend({
+		model: Question
+
+	});
+	return Questions;
+});
+
+
+
+
+
+
+
